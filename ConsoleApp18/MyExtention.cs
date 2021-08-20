c714ad8c48d5a5f90d44835b0a5ba1a098d1ee0e
Паша Луковников <lukovnikov.pasha@gmail.com>
2021-08-20 16:54:47 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp18/MyExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    static class MyExtention
+    {
+        public static string PrintFullName(this Student student)
+        {
+            return student.FirstName + " " + student.LastName;
+        }
+
+        //public static void Print(this DateTime dateTime)
+        //{
+        //    Console.WriteLine(dateTime);
+        //}
+
+        //public static bool IsDayOfWeek(this DateTime dateTime, DayOfWeek dayOfWeek)
+        //{
+        //    return dateTime.DayOfWeek == dayOfWeek;
+        //}
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Point point = new Point(3, 5);
+            //point.Print();
+            Student student = new Student() { FirstName = "Den", LastName = "jopa"};
+            string fullName = student.PrintFullName();
+            Console.WriteLine(fullName);
+            //DateTime dateTime = DateTime.Now;
+            //dateTime.Print();
+            //Console.WriteLine(dateTime.IsDayOfWeek(DayOfWeek.Friday));
+        }
+    }
+
+    
+    //class Point
+    //{
+    //    public Point()
+    //    {
+    //        _x = _y = 1;
+    //    }
+
+    //    public Point(int x, int y)
+    //    {
+    //        _x = x;
+    //        _y = y;
+    //    }
+
+    //    private int _x;
+    //    private int _y;
+
+    //    public void Print()
+    //    {
+    //        Console.WriteLine($"X: {_x}\tY: {_y}");
+    //    }
+    //}
+}
